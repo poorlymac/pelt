d9da64fb1e3e696f446cfe9536158899a0151227
Paul Schaap <ps@ipggroup.com>
2019-03-07 10:54:03 +1100

    Static'ish build option to ease distribution

--- build.sh
+++ build.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-gcc -o pelt -lconfig -lpq -lmysqlclient -L/usr/local/mysql/lib -I/usr/local/mysql/include pelt.c
+if [ "$1" == "static" ]
+then
+    # A static compile if you want to distribute
+    # Note using homebrew libraries
+    gcc -O3 -o pelt \
+        pelt.c postgres.c extract.c \
+        /usr/local/lib/libpq.a \
+        /usr/local/lib/libconfig.a \
+        /usr/local/mysql/lib/libmysqlclient.a \
+        /usr/local/Cellar/openssl/1.0.2q/lib/libcrypto.a \
+        /usr/local/Cellar/openssl/1.0.2q/lib/libssl.a \
+        -lldap \
+        -lkrb5 \
+        -I/usr/local/mysql/include
+else
+    gcc -O3 -o pelt -lconfig -lpq -lmysqlclient -L/usr/local/mysql/lib -I/usr/local/mysql/include pelt.c postgres.c extract.c
+fi
